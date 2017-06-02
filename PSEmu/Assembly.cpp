26200d4021c656acc3eb02dd0ed7451d9d5a43c6
l <@>
2017-06-02 21:08:12 +0200

    Assembly: added loads and stores

--- PSEmu/Assembly.cpp
+++ PSEmu/Assembly.cpp
@@ -1,6 +1,5 @@
 #include "Assembly.h"
-
-
+#include <map>
 
 Assembly::Assembly()
 {
@@ -10,3 +9,84 @@ Assembly::Assembly()
 Assembly::~Assembly()
 {
 }
+
+Assembly& Assembly::nop()
+{
+	this->push_back(0);
+	return *this;
+}
+
+
+
+Assembly & Assembly::lb(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x20 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::lbu(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x24 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::lh(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x21 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::lhu(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x25 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::lw(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x23 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::lwr(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x26 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::sb(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x28 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::sh(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x29 << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::swl(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x2a << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::sw(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x2b << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+Assembly & Assembly::swr(Register rs, Register rt, uint16_t imm)
+{
+	this->push_back((0x2e << 26) + (RNO(rs) << 21) + (RNO(rt) << 16) + imm);
+	return *this;
+}
+
+uint8_t Assembly::RNO(Register r)
+{
+	return static_cast<uint8_t>(r);
+}
+
+
--- PSEmu/Assembly.h
+++ PSEmu/Assembly.h
@@ -2,9 +2,67 @@
 #include <cstdint>
 #include <vector>
 
+enum class Register : uint8_t;
 class Assembly: public std::vector<uint32_t>
 {
 public:
 	Assembly();
 	~Assembly();
+
+	Assembly& nop();
+
+	Assembly& lb(Register rs, Register rt, uint16_t imm);
+	Assembly& lbu(Register rs, Register rt, uint16_t imm);
+	Assembly& lh(Register rs, Register rt, uint16_t imm);
+	Assembly& lhu(Register rs, Register rt, uint16_t imm);
+	Assembly& lw(Register rs, Register rt, uint16_t imm);
+	Assembly& lwr(Register rs, Register rt, uint16_t imm);
+
+	Assembly& sb(Register rs, Register rt, uint16_t imm);
+	Assembly& sh(Register rs, Register rt, uint16_t imm);
+	Assembly& swl(Register rs, Register rt, uint16_t imm);
+	Assembly& sw(Register rs, Register rt, uint16_t imm);
+	Assembly& swr(Register rs, Register rt, uint16_t imm);
+
+
+
+private:
+	uint8_t RNO(Register r);
+
+	Assembly& load(const char* which, Register rs, Register rt, uint16_t imm);
+};
+
+enum class Register : uint8_t {
+	R00,
+	R01,
+	R02,
+	R03,
+	R04,
+	R05,
+	R06,
+	R07,
+	R08,
+	R09,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	R15,
+	R16,
+	R17,
+	R18,
+	R19,
+	R20,
+	R21,
+	R22,
+	R23,
+	R24,
+	R25,
+	R26,
+	R27,
+	R28,
+	R29,
+	R30,
+	R31
 };
