e8079a7a777d2191a26cd352bc510f73f8552b27
Leonardo Bartoloni <pqnet@robotsanta>
2017-06-07 09:42:42 +0200

    add some function code to test

--- TestEmulator/TestEmulator.cpp
+++ TestEmulator/TestEmulator.cpp
@@ -10,11 +10,12 @@ using R = Register;
 
 int main()
 {
-	constexpr size_t memory_size = 1024 * 1024 * 2;
+	constexpr size_t memory_size = 0x200000;
 	constexpr uint32_t code_base_addr = 0xf0000000;
 	auto memory = std::make_shared<MemoryBlock<memory_size>>();
 	auto code = std::make_shared<Assembly>();
 
+	/*
 	(*code)
 		.nop()
 		.lw(R(1), R(0), 0)
@@ -25,12 +26,53 @@ int main()
 		.lhu(R(6), R(0), 4)
 		.jal(0)
 		;
+		*/
+	/*
+		0  : 27bdfff8        addiu   sp, sp, -8
+		4  : afbe0004        sw      s8, 4(sp)
+		8  : 03a0f025        move    s8, sp
+		c  : 8c810000        lw      at, 0(a0)
+		10 : 8c82000c        lw      v0, 12(a0)
+		14 : 00410821        addu    at, v0, at
+		18 : ac810008        sw      at, 8(a0)
+		1c : 24220001        addiu   v0, at, 1
+		20 : 03c0e825        move    sp, s8
+		24 : 8fbe0004        lw      s8, 4(sp)
+		28 : 03e00008        jr      ra
+		2c : 27bd0008        addiu   sp, sp, 8
+		*/
+	(*code)
+		.label("begin")
+		.nop()
+		.addiu(R::SP, R::SP, -8) //allocate stack space
+		.sw(R::FP, R::SP, 4) // save frame pointer
+		.move(R::FP, R::SP) // save stack pointer
+		.lw(R::AT, R::A0, 0)  // at = arg0[0];
+		.lw(R::V0, R::A0, 12) // v0 = arg0[3];
+		.addu(R::AT, R::V0, R::AT) // at+=v0;
+		.sw(R::AT, R::A0, 8) // arg0[2] = at;
+		.addiu(R::V0, R::AT, 1) // v0 = at+1;
+		.move(R::SP, R::FP) //restore stack pointer
+		.lw(R::FP, R::SP, 4) //restore frame pointer
+		.jr(R::RA) //return from function (executes next first)
+		.addiu(R::SP, R::SP, 8) // deallocate stack space
+		.nop()
+	;
+	(*code)
+		.label("main")
+		.nop()
+		.li(R::SP, uint32_t(memory_size - 4))
+		.li(R::FP, uint32_t(memory_size))
+		.jal((code->labelAddr("begin") + code_base_addr) / 4)
+		.li(R::A0, 0x0)
+		.sw(R::V0, R(0), 12)
+		;
 
 	auto mmap = std::make_shared<MemoryMap>();
 	mmap->add_map(memory, 0, memory_size, 0, 0);
 	mmap->add_map(code, code_base_addr, code->size()*4, 0, 0);
 	Cpu cpu(mmap,nullptr,nullptr,nullptr,nullptr);
-	cpu.Reset(code_base_addr);
+	cpu.Reset(code_base_addr+code->labelAddr("main"));
 
 	mmap->store32(0, 0xfefefefe);
 	mmap->store32(4, 0x10cacca);
